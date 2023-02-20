32e1750f05b37f5b227d95b874b87e8fe9b9ce61
Dmitry Grigorev <d-d12@mail.ru>
2023-02-20 11:18:18 +0400

    Добавьте файлы проекта.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using WindowsFormsApplication396.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //создаем опросник
+            var questionnaire = new Questionnaire();
+            //создаем вопрос
+            var quest = new Quest { Title = "Заголовок вопроса" };
+            //создаем альтренативу
+            var alt = new Alternative {Code = 1, Title = "Вариант1"};
+            //добавляем альтернативу в вопрос
+            quest.Add(alt);
+            //добавляем вопрос в опросник
+            questionnaire.Add(quest);
+
+            //сохраняем опросник в файл
+            SaverLoader.Save(questionnaire, "F:\\temp.q");
+
+            //читаем опросник из файла
+            var loadedQuestionnaire = SaverLoader.Load<Questionnaire>("F:\\temp.q");
+
+            //проверяем число вопросов и альтернатив в загруженном опроснике
+            Assert.AreEqual(loadedQuestionnaire.Count, questionnaire.Count);
+            Assert.AreEqual(loadedQuestionnaire[0].Count, questionnaire[0].Count);
+        }
+    }
+}
---
+++ WindowsFormsApplication396/Core/Questionnaire.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication396.Core
+{
+    /// <summary>
+    /// Опросник.
+    /// Содержит список вопросов.
+    /// </summary>
+    [Serializable]
+    public class Questionnaire : List<Quest>
+    {
+    }
+
+    /// <summary>
+    /// Вопрос.
+    /// Содержит список альтернатив.
+    /// </summary>
+    [Serializable]
+    public class Quest : List<Alternative>
+    {
+        /// <summary>
+        /// Идентификатор вопроса
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Текст вопроса
+        /// </summary>
+        public string Title { get; set; }
+    }
+
+    /// <summary>
+    /// Альтернатива вопроса
+    /// </summary>
+    [Serializable]
+    public class Alternative
+    {
+        /// <summary>
+        /// Код альтернативы
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// Текст альтернативы
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
